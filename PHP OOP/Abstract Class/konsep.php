1e45ad88fc1ab6ff7a709f07eb98117a6df7770f
altamiz <60939569+abdrhmnn@users.noreply.github.com>
2020-07-25 22:55:17 +0700

    konsep abstract method

--- PHP OOP/Abstract Class/konsep.php
+++ PHP OOP/Abstract Class/konsep.php
@@ -3,3 +3,4 @@
 // Abstract class adalah sebuah class yang tidak dapat di instansiasi, jadi tidak bisa bikin object
 // dari class ini dan harus memiliki minimal 1 method abstrak yang mendefinisikan interface dan sebagai kerangka
 // dasar untuk class turunannya (inheritance)
+
